ba409c650c8b3e80225fc3e2b169bd47a7f925ff
Matt Vogel <mainmoniker@googlemail.com>
2022-10-19 19:55:40 -0400

    init

--- extension.js
+++ extension.js
@@ -1,45 +1,91 @@
-const panelConfig = {
-  tabTitle: "Test Ext 1",
-  settings: [
-      {id:          "button-setting",
-       name:        "Button test",
-       description: "tests the button",
-       action:      {type:    "button",
-                     onClick: (evt) => { console.log("Button clicked!"); },
-                     content: "Button"}},
-      {id:          "switch-setting",
-       name:        "Switch Test",
-       description: "Test switch component",
-       action:      {type:     "switch",
-                     onChange: (evt) => { console.log("Switch!", evt); }}},
-      {id:     "input-setting",
-       name:   "Input test",
-       action: {type:        "input",
-                placeholder: "placeholder",
-                onChange:    (evt) => { console.log("Input Changed!", evt); }}},
-      {id:     "select-setting",
-       name:   "Select test",
-       action: {type:     "select",
-                items:    ["one", "two", "three"],
-                onChange: (evt) => { console.log("Select Changed!", evt); }}}
-  ]
-};
+
+function timeButton({ extensionAPI }) {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = React.useState(extensionAPI.settings.get('timer'));
+
+    return (
+        React.createElement(
+            "input",
+            {className: "self-destruct-input bp3-input",
+            type:"number",
+            min:1,
+            value: count,
+            placeholder:count,
+            onChange:	(evt) => { 
+                setCount(evt.target.value)
+                extensionAPI.settings.set('timer', evt.target.value)
+            }},
+        )
+    );
+}
+
+function getPageRefs(pageName) {
+    let queryText = `[:find (pull ?r [:block/uid :create/time])
+        :in $ ?pagename   
+        :where
+        [?p :node/title ?pagename]
+        [?r :block/refs ?p]
+        [?r :block/page ?pr]]`
+    return window.roamAlphaAPI.q(queryText, pageName);
+}
 
 async function onload({extensionAPI}) {
-  // set defaults if they dont' exist
-  if (!extensionAPI.settings.get('data')) {
-      await extensionAPI.settings.set('data', "01");
-  }
-  extensionAPI.settings.panel.create(panelConfig);
+    // set defaults if they dont' exist
+    if (!extensionAPI.settings.get('tag')) {
+        await extensionAPI.settings.set('tag', "self-destruct");
+    }
+    if (!extensionAPI.settings.get('timer')) {
+        await extensionAPI.settings.set('timer', 1);
+    }
 
-  console.log("load example plugin");
+    // let the react component access extensionAPI
+    const wrappedTimeConfig = () => timeButton({ extensionAPI });
+
+    const panelConfig = {
+        tabTitle: "Self-Destruct block",
+        settings: [
+            {id:	 "tag",
+             name:   "Self-destruct tag",
+             action: {type:		"input",
+                      placeholder: "self-destruct",
+                      onChange:	(evt) => { console.log( evt.target.value); }}},
+    
+            {id:	 "timer",
+             name:   "Days until blocks self-destruct",
+             action: {type:	 "reactComponent",
+                      component: wrappedTimeConfig}}
+        ]
+    };
+
+    extensionAPI.settings.panel.create(panelConfig);
+
+    // get all the refs for the self-destruct page
+    let pageRefs = getPageRefs(await extensionAPI.settings.get('tag'))
+
+    // for each block check if it's older than the self-destruct limit. If so delete the block
+    // I only check this on plugin load. This seems far simpler since I assume most people don't keep roam open infinitely 
+    // maybe a bad assumption...
+
+    pageRefs.forEach(block => {
+        let createTime = block[0]['time'];
+        let numDays = extensionAPI.settings.get('timer')
+        let offsetTime = new Date().getTime() - (numDays * 24 * 60 * 60 * 1000)
+        console.log("offset days", numDays, offsetTime)
+        if (createTime<offsetTime) {
+            console.log("delete", block)
+            window.roamAlphaAPI.deleteBlock({"block":{"uid": block[0]['uid']}})
+        }
+        
+    });
+
+    console.log("load self-destruct plugin");
 }
 
 function onunload() {
-  console.log("unload example plugin");
+    console.log("unload self-destruct plugin");
 }
-
+  
 export default {
-onload,
-onunload
+    onload,
+    onunload
 };
